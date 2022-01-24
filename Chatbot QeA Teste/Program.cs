f11c4d82ec4945d04a22d54642678df04089eb9e
Marcelo Pepis <marcelo.pepis@gmail.com>
2022-01-24 16:28:06 -0300

    commit inicial

---
+++ Chatbot QeA Teste/Program.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.15.0
+
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System.Net.Http;
+
+namespace Chatbot_QeA_Teste
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+        private static readonly HttpClient client = new HttpClient();
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
